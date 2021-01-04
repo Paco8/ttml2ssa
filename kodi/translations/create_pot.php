142fe704270091b19761339b00852177b683fccb
Paco8 <5084042+Paco8@users.noreply.github.com>
2021-01-04 16:58:53 +0100

    Import kodi files

---
+++ kodi/translations/create_pot.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php -q
+<?php
+?>
+# XBMC Media Center language file
+# Addon Name: Ttml2Ssa
+# Addon id: script.module.ttml2ssa
+# Addon version: 0.1.1
+# Addon Provider: Paco8
+#, fuzzy
+msgid ""
+msgstr ""
+"Project-Id-Version: XBMC-Addons\n"
+"Report-Msgid-Bugs-To: \n"
+"POT-Creation-Date: 2020-12-28 16:47+0100\n"
+"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
+"Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
+"Language-Team: LANGUAGE\n"
+"Language: en\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+
+<?php
+$n = 32000;
+
+$l = array('SSA Style', 
+           'Font name', 'Font size', 
+           'Primary colour (AABBGGRR)', 'Back colour (AABBGGRR)', 'Outline colour (AABBGGRR)',
+           'Border style', 'Outline', 'Shadow',
+           'Vertical margin', 'Left margin', 'Right margin',
+           'Misc',
+           'Allow cosmetic changes', 'Allow fixes for some languages',
+           'Timestamp minimum separation (SSA only) in ms',
+           'Outline with shadow', 'Opaque box',
+           'Bold', 'Italic');
+
+foreach ($l as $i) {
+	echo "msgctxt \"#$n\"\n";
+	echo "msgid \"$i\"\n";
+	echo "msgstr \"\"\n\n";
+
+	$n++;
+}
+?>
